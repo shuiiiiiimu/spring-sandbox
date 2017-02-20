01e441879c46ed9ab53360ce3770ac4973a2b22d
zhangshuimu <765732980@qq.com>
2017-02-20 22:30:11 +0800

    inject config

--- src/main/java/com/sandbox/controller/Hello.java
+++ src/main/java/com/sandbox/controller/Hello.java
@@ -2,6 +2,7 @@ package com.sandbox.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,9 @@ public class Hello {
 
     private static final Logger logger =  LoggerFactory.getLogger(Hello.class);
 
+    @Value("${hello.name}")
+    private String name;
+
     @RequestMapping("/")
     @ResponseBody
     public Map<String, Object> sayHi(){
@@ -26,6 +30,11 @@ public class Hello {
         result.put("a", 11);
         result.put("b", "hello");
         return result;
+    }
+
+    @RequestMapping("/iv")
+    public String injectValue(){
+        return String.format("hello, %s", name);
     }
 
 }
