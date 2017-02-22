46cabd0d89ba2bc9c452dd76dac172cff300741d
zhangshuimu <765732980@qq.com>
2017-02-22 21:55:57 +0800

    add retrofit2

--- src/main/java/com/sandbox/controller/Hello.java
+++ src/main/java/com/sandbox/controller/Hello.java
@@ -1,11 +1,18 @@
 package com.sandbox.controller;
 
+import com.sandbox.service.PullWaveService;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -35,6 +42,32 @@ public class Hello {
     @RequestMapping("/iv")
     public String injectValue(){
         return String.format("hello, %s", name);
+    }
+
+    @RequestMapping("/git")
+    public String fetchGitUsers() {
+        String result = "Retrofit.test";
+
+        OkHttpClient okhttpClient = new OkHttpClient();
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+        okhttpClient = okhttpClient.newBuilder().addInterceptor(logging).build();
+        Retrofit retrofit = new Retrofit.Builder()
+                .client(okhttpClient)
+                .baseUrl("http://pullwave.com/")
+                .build();
+        PullWaveService pullWaveService = retrofit.create(PullWaveService.class);
+        Call<okhttp3.ResponseBody> call = pullWaveService.wave("CEO");
+        call.enqueue(new Callback<okhttp3.ResponseBody>() {
+            @Override
+            public void onResponse(Call<okhttp3.ResponseBody> call, Response<okhttp3.ResponseBody> response) {
+                logger.debug(String.format("Retrofit Response => %s", response));
+                logger.info(String.format("Body => %s", response.body()));
+            }
+            @Override
+            public void onFailure(Call<okhttp3.ResponseBody> call, Throwable t) {}
+        });
+        return result;
     }
 
 }
---
+++ src/main/java/com/sandbox/service/PullWaveService.java
@@ -0,0 +1,15 @@
+package com.sandbox.service;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+/**
+ * Created by mike on 2017/2/22.
+ */
+public interface PullWaveService {
+
+    @GET("get.php?json=1&auth_usr=free_vi") // -> get.php?json=1&auth_usr=free_vip&w1=%E9%98%B4%E9%98%B3%E5%B8%88&w2=&end_date=2016-12-27
+    Call<ResponseBody> wave(@Query("w1") String word);
+}
